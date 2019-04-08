b587d0170107acf76feebc43d6d5aa78bef42144
Ilario Coppola <ilario.coppola@studio.unibo.it>
2019-04-08 10:52:15 +0200

    Update ripple_carry_adder_beh.sv

--- ripple_carry_add_beh/ripple_carry_adder_beh.sv
+++ ripple_carry_add_beh/ripple_carry_adder_beh.sv
@@ -5,23 +5,28 @@ module ripple_carry_adder_beh
 	input logic	  CIN,
 
 	output logic [2:0] SUM,
-	output logic	   COUT
+	output logic [2:0] COUT
 );
 
 logic INT0,INT1;
 
-	//Can be replaced by a for loop	
+	/* ------------------------------------------------------------------------- 
+	for(int i=1; i<3; i++) begin                                                 
+		assign SUM[i] = A[i] ^ B[i] ^ COUT[i];                               
+		assign COUT[i] = (A[i] & B[i]) | (COUT[i] & A[i]) | (COUT[i] & B[i]);
+	end                                                                          
+	---------------------------------------------------------------------------*/
 	
 	assign SUM[0] = A[0] ^ B[0] ^ CIN;
 
-	assign INT0 = (A[0] & B[0])|(CIN & A[0])|(CIN & B[0]);
+	assign COUT[0] = (A[0] & B[0]) | (CIN & A[0]) | (CIN & B[0]);
 
-	assign SUM[1] = A[1] ^ B[1] ^ INT0;
+	assign SUM[1] = A[1] ^ B[1] ^ COUT[0];
 
-	assign INT1 = (A[1] & B[1])|(INT0 & A[1])|(INT0 & B[1]);
+	assign COUT[1] = (A[1] & B[1]) | (COUT[0] & A[1]) | (COUT[0] & B[1]);
 
-	assign SUM[2] = A[2] ^ B[2] ^ INT1;
+	assign SUM[2] = A[2] ^ B[2] ^ COUT[1];
 
-	assign COUT = (A[2] & B[2])|(INT1 & A[2])|(INT1 & B[2]);
+	assign COUT[2] = (A[2] & B[2]) | (COUT[1] & A[2]) | (COUT[1] & B[2]);
 
 endmodule
