3e3a7c620b1d29ad04a579b089f9faa4daf534b9
Ilario Coppola <ilario.coppola@studio.unibo.it>
2019-04-09 19:58:59 +0200

    Update ripple_carry_add_beh

--- ripple_carry_add_beh/ripple_carry_adder_beh.sv
+++ ripple_carry_add_beh/ripple_carry_adder_beh.sv
@@ -8,27 +8,13 @@ module ripple_carry_adder_beh
 	output logic [3:0] COUT
 );
 
+	genvar i;
 
-	/* ------------------------------------------------------------------------- 
-	for(int i=1; i<3; i++) begin                                                 
-		assign SUM[i] = A[i] ^ B[i] ^ COUT[i];                               
-		assign COUT[i] = (A[i] & B[i]) | (COUT[i-1] & (A[i] ^ B[i]));
-	end                                                                          
-	---------------------------------------------------------------------------*/
-	
 	assign SUM[0] = A[0] ^ B[0] ^ CIN;
 
 	assign COUT[0] = (A[0] & B[0]) | (CIN & (A[0] ^ B[0]));
-
-	//assign SUM[1] = A[1] ^ B[1] ^ COUT[0];
-
-	//assign COUT[1] = (A[1] & B[1]) | (COUT[0] & (A[1] ^ B[1]));
-
-	//assign SUM[2] = A[2] ^ B[2] ^ COUT[1];
-
-	//assign COUT[2] = (A[2] & B[2]) | (COUT[1] & (A[2] ^ B[2]));
 	
-	for(int i=1; i<4; i++) begin                                                 
+	for(i=1; i<4; i++) begin                                                 
 		assign SUM[i] = A[i] ^ B[i] ^ COUT[i-1];                               
 		assign COUT[i] = (A[i] & B[i]) | (COUT[i-1] & (A[i] ^ B[i]));
 	end
