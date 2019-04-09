712c25031c7f09db80b20a70a154c769cbf48572
Ilario Coppola <ilario.coppola@studio.unibo.it>
2019-04-09 19:49:10 +0200

    Update testbench.sv

--- ripple_carry_add_beh/testbench.sv
+++ ripple_carry_add_beh/testbench.sv
@@ -3,31 +3,32 @@
 
 module testbench ();
    
-   logic [2:0] A;
-   logic [2:0] B;
-   logic [2:0] SUM;
-   logic       CIN,COUT;
+   logic [3:0] A;
+   logic [3:0] B;
+   logic [3:0] SUM;
+   logic [3:0] COUT;
+   logic       CIN;
    
    ripple_carry_adder_beh dut ( .A(A), .B(B), .CIN(CIN), .SUM(SUM), .COUT(COUT) );
    
    initial
      begin
         
-        A   = 0;
-        B   = 0;
-        CIN = 0;
+        A   = 4'd0;
+        B   = 4'd0;
+        CIN = 4'd0;
         
         #2;
         
-        A   = 7;
-        B   = 3;
-        CIN = 0;
+        A   = 4'd7;
+        B   = 4'd3;
+        CIN = 4'd0;
         
         #2;
         
-        A   = 2;
-        B   = 1;
-        CIN = 0;
+        A   = 4'd2;
+        B   = 4'd1;
+        CIN = 4'd0;
         
         #2;
 
